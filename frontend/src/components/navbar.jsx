79e8fb94dd307f25218339c6011017ffb3d88206
Nghiem Chi Bao <19521243@gm.uit.edu.vn>
2023-03-09 22:23:59 +0700

    Update NavBar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,26 +1,38 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import '../scss/NavBar.scss'
+import Section from '../assets/Data/Section.json'
 
 function NavBar() {
   return (
-    <Navbar bg="light" expand="lg" sticky="top">
+    <Navbar className='NavBar' collapseOnSelect expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Trang chủ</Nav.Link>
-            <Nav.Link href="#link">Shop</Nav.Link>
-            <Nav.Link href="#link">Đặc trưng</Nav.Link>
-            <Nav.Link href="#link">Blog</Nav.Link>
-            <Nav.Link href="#link">Về chúng tôi</Nav.Link>
-            <Nav.Link href="#link">Liên hệ</Nav.Link>
+        <Navbar.Brand href="#home">
+            <img src='Images/logo.webp' alt='logo'/>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="Nav_Section">
+            {Section.map((section)=>(
+                <Nav.Link>{section}</Nav.Link>
+            ))}
           </Nav>
         </Navbar.Collapse>
+        <Nav>
+            <Nav.Link href="#deets">
+                <img className='icon' src="Images/Icons/searchIcon.png" alt="Search"/>
+            </Nav.Link>
+            <Nav.Link href="#memes">
+              <img className='icon' src="Images/Icons/cartIcon.png" alt="Cart" />
+            </Nav.Link>
+            <Nav.Link href="#memes">
+              <img className='icon' src="Images/Icons/favoriteIcon.png" alt="Cart" />
+            </Nav.Link>
+          </Nav>
       </Container>
     </Navbar>
   );
 }
 
-export default NavBar; 
+export default NavBar;
