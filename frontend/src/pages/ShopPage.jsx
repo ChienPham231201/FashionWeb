d225fcc972bf150c8bbc3e15cfa072d0e14eae0f
Nghiem Chi Bao <19521243@gm.uit.edu.vn>
2023-03-25 13:42:11 +0700

    update Shop Page

--- frontend/src/pages/ShopPage.jsx
+++ frontend/src/pages/ShopPage.jsx
@@ -1,6 +1,27 @@
+import ProductCard from '../components/ProductCard';
+import Container from 'react-bootstrap/esm/Container';
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/esm/Col';
+import "../scss/ShopPage.scss"
+
 const ShopPage = () => {
     return (
-        <p>Hello</p>
+        <Container fluid className='ShopPage'>
+            <Row className="home_layout-row">
+                    <Col lg={3} md={6} className="home_layout-col">
+                        <ProductCard />
+                    </Col>
+                    <Col lg={3} md={6} className="home_layout-col">
+                        <ProductCard />
+                    </Col>
+                    <Col lg={3} md={6} className="home_layout-col">
+                        <ProductCard />
+                    </Col>
+                    <Col lg={3} md={6} className="home_layout-col">
+                        <ProductCard />
+                    </Col>
+                </Row>
+        </Container>
     );
-}
-export default ShopPage
+};
+export default ShopPage;
