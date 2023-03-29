06bd37379aa8648413f55021d056bdc881702d1d
Nghiem Chi Bao <19521243@gm.uit.edu.vn>
2023-03-29 14:53:05 +0700

    update About Page and Contact Page

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,5 +1,8 @@
-import "../scss/AboutPage.scss"
+import '../scss/AboutPage.scss';
 import { about1, about2 } from '../assets/img';
+import Container from 'react-bootstrap/esm/Container';
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/esm/Col';
 
 const AboutPage = () => {
     return (
@@ -7,27 +10,39 @@ const AboutPage = () => {
             <div className="BackGround">
                 <p className="Name">Về chúng tôi</p>
             </div>
-            <div>
-                <h1>Cau chuyen</h1>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Sit eligendi sunt veritatis facere, repellat qui magnam,
-                    commodi, sint repellendus expedita sed tempora ratione earum
-                    dolorem! Necessitatibus at laborum inventore similique.
-                </p>
-                <img src={about1} alt="cc" ></img>
-            </div>
-            <div>
-                <h1>Cau chuyen</h1>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Sit eligendi sunt veritatis facere, repellat qui magnam,
-                    commodi, sint repellendus expedita sed tempora ratione earum
-                    dolorem! Necessitatibus at laborum inventore similique.
-                </p>
-                <img src={about2} alt="cc" ></img>
-            </div>
+            <Container>
+                <Row>
+                    <Col>
+                        <h1>Cau chuyen</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing
+                            elit. Sit eligendi sunt veritatis facere, repellat
+                            qui magnam, commodi, sint repellendus expedita sed
+                            tempora ratione earum dolorem! Necessitatibus at
+                            laborum inventore similique.
+                        </p>
+                    </Col>
+                    <Col>
+                    <img src={about1} alt="cc"></img>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <h1>Cau chuyen</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing
+                            elit. Sit eligendi sunt veritatis facere, repellat
+                            qui magnam, commodi, sint repellendus expedita sed
+                            tempora ratione earum dolorem! Necessitatibus at
+                            laborum inventore similique.
+                        </p>
+                    </Col>
+                    <Col>
+                    <img src={about2} alt="cc"></img>
+                    </Col>
+                </Row>
+            </Container>
         </>
-    )
-}
+    );
+};
 export default AboutPage;
--- frontend/src/pages/ContactPage.jsx
+++ frontend/src/pages/ContactPage.jsx
@@ -1,4 +1,7 @@
-import { about1, about2 } from '../assets/img';
+import { Button } from 'bootstrap';
+import Col from 'react-bootstrap/esm/Col';
+import Container from 'react-bootstrap/esm/Container';
+import Row from 'react-bootstrap/esm/Row';
 import '../scss/ContactPage.scss';
 
 const ContactPage = () => {
@@ -7,16 +10,14 @@ const ContactPage = () => {
             <div className="BackGround">
                 <p className="Name">Liên hệ</p>
             </div>
-            <div>
-                <h1>Cau chuyen</h1>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Sit eligendi sunt veritatis facere, repellat qui magnam,
-                    commodi, sint repellendus expedita sed tempora ratione earum
-                    dolorem! Necessitatibus at laborum inventore similique.
-                </p>
-                <img src={about1} alt="cc" ></img>
-            </div>
+            <Container>
+                <Row>
+                    <Col>
+                    <h2>Gui thu cho chung toi</h2>
+                    </Col>
+                    <Col></Col>
+                </Row>
+            </Container>
         </>
     );
 };
