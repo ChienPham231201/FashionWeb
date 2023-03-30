9c02ba33dcd7251bec082ab17a6d887cc271b6db
Nghiem Chi Bao <19521243@gm.uit.edu.vn>
2023-03-30 22:24:32 +0700

    update AboutPage and ContactPage

--- frontend/src/pages/ContactPage.jsx
+++ frontend/src/pages/ContactPage.jsx
@@ -1,8 +1,12 @@
-import { Button } from 'bootstrap';
+import Button from 'react-bootstrap/esm/Button';
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Col from 'react-bootstrap/esm/Col';
 import Container from 'react-bootstrap/esm/Container';
 import Row from 'react-bootstrap/esm/Row';
 import '../scss/ContactPage.scss';
+
+import { mailIcon, locationIcon, phoneIcon } from '../assets/img';
 
 const ContactPage = () => {
     return (
@@ -12,10 +16,51 @@ const ContactPage = () => {
             </div>
             <Container>
                 <Row>
-                    <Col>
-                    <h2>Gui thu cho chung toi</h2>
+                    <Col className='Form'>
+                        <h2>Gửi thư cho chúng tôi</h2>
+
+                        <FloatingLabel
+                            controlId="floatingInput"
+                            label="Email của bạn"
+                            className="mb-3"
+                        >
+                            <Form.Control
+                                type="email"
+                                placeholder="name@example.com"
+                            />
+                        </FloatingLabel>
+
+                        <FloatingLabel
+                            controlId="floatingPassword"
+                            label="Password"
+                        >
+                            <Form.Control
+                                as="textarea"
+                                placeholder="Ý kiến của bạn"
+                            />
+                        </FloatingLabel>
+
+                        <Button className="submit">Gửi</Button>
                     </Col>
-                    <Col></Col>
+                    <Col>
+                        <div className='Location'>
+                            <img src={locationIcon} alt="location icon" />
+                            <h2>Địa chỉ</h2>
+                        </div>
+
+                        <p>Trường Đại học công nghệ thông tin</p>
+
+                        <div className='Phone'>
+                            <img src={phoneIcon} alt="location icon" />
+                            <h2>Hãy gọi cho chúng tôi</h2>
+                        </div>
+                        <p>+012345678</p>
+                        <div className='Mail'>
+                            <img src={mailIcon} alt="mail icon" />
+                            <h2>Hỗ trợ</h2>
+                        </div>
+                        <p>example@example.com</p>
+                    </Col>
                 </Row>
             </Container>
         </>
